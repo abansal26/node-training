49d27bbc7e17b74bfa261b8f0b29e2d0c364fe83
Akshay Bansal <abansal@grepruby.com>
2017-08-29 15:38:39 +0530

    changed index to iteration

--- flatten.js
+++ flatten.js
@@ -1,13 +1,14 @@
 // Problem 3: Complete the flatten function that takes a JS Object, returns a JS Object in flatten format (compressed)
 function flatten(unflatObject) {
   var output = {};
-  for (var index in unflatObject) {
-    if (typeof unflatObject[index] == "object") {
-      var flattenObject = flatten(unflatObject[index]);
+  for (var iteration in unflatObject) {
+    if (typeof unflatObject[iteration] == "object") {
+      var flattenObject = flatten(unflatObject[iteration]);
       for (var count in flattenObject) {
-        output[index + "." + count] = flattenObject[count];
+        output[iteration + "." + count] = flattenObject[count];
       }
-    } else output[index] = unflatObject[index];
+    } else 
+        output[iteration] = unflatObject[iteration];
   }
   return output;
 }
