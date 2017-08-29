18ed281bafc189d2ad80b2e3ae010d3037b735bc
Akshay Bansal <abansal@grepruby.com>
2017-08-29 18:57:15 +0530

    programs of first assignment are now implemented in node with file server.js

---
+++ programs/flatten.js
@@ -0,0 +1,15 @@
+// Problem 3: Complete the flatten function that takes a JS Object, returns a JS Object in flatten format (compressed)
+function flatten(unflatObject) {
+  var output = {};
+  for (var iteration in unflatObject) {
+    if (typeof unflatObject[iteration] == "object") {
+      var flattenObject = flatten(unflatObject[iteration]);
+      for (var count in flattenObject) {
+        output[iteration + "." + count] = flattenObject[count];
+      }
+    } else 
+        output[iteration] = unflatObject[iteration];
+  }
+  return output;
+}
+exports.flatten = flatten;
