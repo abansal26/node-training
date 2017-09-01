7d14d68f46d3e518a3790a66a960b588633c337d
Akshay Bansal <abansal@grepruby.com>
2017-09-01 12:17:34 +0530

    Var promise removed as well as reject is returned and .catch is also added

--- assignment6/promisesassignment.js
+++ assignment6/promisesassignment.js
@@ -1,56 +1,55 @@
 var fs = require("fs");
 
 var firstMethod = function() {
-  var promise = new Promise(function(resolve, reject) {
+  return new Promise(function(resolve, reject) {
     fs.readFile("file1.txt", function(err, data) {
       if (err) {
-        reject(err);
+        return reject(err);
       }
       console.log("first method completed");
       resolve(data);
     });
   });
-  return promise;
 };
 
 var secondMethod = function(data) {
-  var promise = new Promise(function(resolve, reject) {
+  return new Promise(function(resolve, reject) {
     fs.appendFile("file2.txt", data, function(err) {
       if (err) {
-        reject(err);
+        return reject(err);
       }
       resolve(console.log("second method completed"));
     });
   });
-  return promise;
 };
 
 var thirdMethod = function() {
-  var promise = new Promise(function(resolve, reject) {
+  return new Promise(function(resolve, reject) {
     fs.readFile("file2.txt", function(err, data) {
       if (err) {
-        reject(err);
+        return reject(err);
       }
       console.log("third method completed");
       resolve(data);
     });
   });
-  return promise;
 };
 
 var fourthMethod = function(data) {
-  var promise = new Promise(function(resolve, reject) {
+  return new Promise(function(resolve, reject) {
     fs.appendFile("file3.txt", data, function(err) {
       if (err) {
-        reject(err);
+        return reject(err);
       }
       resolve(console.log("fourth method completed"));
     });
   });
-  return promise;
 };
 
 firstMethod()
-.then(secondMethod)
-.then(thirdMethod)
-.then(fourthMethod);
+  .then(secondMethod)
+  .then(thirdMethod)
+  .then(fourthMethod)
+  .catch(function(e) {
+  console.log(e); 
+  });
