2b780672efb014cb63fc8cff825b998ae65d0206
Akshay Bansal <abansal@grepruby.com>
2017-09-06 10:55:04 +0530

    Changed variable rp to requestPromise and indentaion is improved

--- assignment7/createClient.js
+++ assignment7/createClient.js
@@ -1,24 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var rp = require('request-promise')
+var requirePromise = require("request-promise");
 
-router.get('/',function(req, res){
-	var options = {
-		'url': 'https://api.createsend.com/api/v3.1/clients.json',
-     	'auth': {
-     		'user' : 'fc96dc9ded698bbfbfb975409a864a42',
-			'pass' : 'x'
-     	},
-  		'body': { 
-  			'CompanyName': 'animesh',
-     		'Country': 'India',
-    		'TimeZone': '(GMT+05:30) Chennai, Kolkata, Mumbai, New Delhi' },
-  		'json': true 
-  		};
-	rp.post(options)
-		.then(function(response){
-		res.send('ID of new Client is '+response);
-		});
+router.get("/", function(req, res) {
+  var options = {
+    url: "https://api.createsend.com/api/v3.1/clients.json",
+    auth: {
+      user: "fc96dc9ded698bbfbfb975409a864a42",
+      pass: "x"
+    },
+    body: {
+      CompanyName: "animesh",
+      Country: "India",
+      TimeZone: "(GMT+05:30) Chennai, Kolkata, Mumbai, New Delhi"
+    },
+    json: true
+  };
+  requirePromise.post(options).then(function(response) {
+    res.send("ID of new Client is " + response);
+  });
 });
 
 module.exports = router;
--- assignment7/deleteClient.js
+++ assignment7/deleteClient.js
@@ -1,22 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var rp = require('request-promise')
+var requirePromise = require("request-promise");
 
-router.get('/:idClient',function(req, res){
-	var idClient = req.params.idClient;
-	var url = 'https://api.createsend.com/api/v3.1/clients/' + idClient + '.json';
-	rp.delete(url,{
-		'auth' : {
-			'user' : 'fc96dc9ded698bbfbfb975409a864a42',
-			'pass' : 'x'
-		}
-	})
-		.then(function(response){
-			res.send('Client with client ID : '+ idClient + ' deleted');
-		})
-		.catch(function(err){
-			console.log("error");
-		});
+router.get("/:idClient", function(req, res) {
+  var idClient = req.params.idClient;
+  var url = "https://api.createsend.com/api/v3.1/clients/" + idClient + ".json";
+  requirePromise
+    .delete(url, {
+      auth: {
+        user: "fc96dc9ded698bbfbfb975409a864a42",
+        pass: "x"
+      }
+    })
+    .then(function(response) {
+      res.send("Client with client ID : " + idClient + " deleted");
+    })
+    .catch(function(err) {
+      console.log("error");
+    });
 });
 
 module.exports = router;
