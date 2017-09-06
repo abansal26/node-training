7d0e8c1047bdc99eb339269df98117ce0e400670
Akshay Bansal <abansal@grepruby.com>
2017-09-06 11:18:03 +0530

    Improved indentation, used console.log seperately and used arrow operator in catch block also

--- assignment6/promisesassignment.js
+++ assignment6/promisesassignment.js
@@ -6,38 +6,40 @@ var fileRead = function(fileName) {
       if (err) {
         return reject(err);
       }
-      console.log("reading "+ fileName +" completed");
+      console.log("reading " + fileName + " completed");
       resolve(data);
     });
   });
 };
 
-var fileAppend = function(data , fileName) {
+var fileAppend = function(data, fileName) {
   return new Promise(function(resolve, reject) {
     fs.appendFile(fileName, data, function(err) {
       if (err) {
         return reject(err);
       }
-      resolve(console.log("Appending data in "+ fileName +" completed"));
+      console.log("Appending data in " + fileName + " completed");
+      resolve();
     });
   });
 };
 
-var fileWrite = function(data , fileName) {
+var fileWrite = function(data, fileName) {
   return new Promise(function(resolve, reject) {
     fs.writeFile(fileName, data, function(err) {
       if (err) {
         return reject(err);
       }
-      resolve(console.log("writing data in "+ fileName +" completed"));
+      console.log("writing data in " + fileName + " completed");
+      resolve();
     });
   });
 };
 
 fileRead("file1.txt")
-  .then(data => fileAppend(data , "file2.txt"))
+  .then(data => fileAppend(data, "file2.txt"))
   .then(data => fileRead("file2.txt"))
-  .then(data => fileWrite(data , "file3.txt"))
-  .catch(function(e) {
-  console.log(e); 
+  .then(data => fileWrite(data, "file3.txt"))
+  .catch(data => function(e) {
+    console.log(e);
   });
