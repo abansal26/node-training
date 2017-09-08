af0892cbf5e8d58d9dcadcab1600257b331f1852
Akshay Bansal <abansal@grepruby.com>
2017-09-08 19:48:40 +0530

    Replaced double qoutes by single qoutes as suggested by @sakodiya

--- assignment 5/nodecallback.js
+++ assignment 5/nodecallback.js
@@ -1,38 +1,38 @@
-var fs = require("fs");
+var fs = require('fs');
 
-fs.readFile("file1.txt", function(err, data) {
+fs.readFile('file1.txt', function(err, data) {
   if (err) {
     return console.error(err);
   }
-  console.log("Reading file 1 complete");
+  console.log('Reading file 1 complete');
   writefile2(data);
 });
 
 var writefile2 = function(data) {
-  fs.appendFile("file2.txt", data, function(err) {
+  fs.appendFile('file2.txt', data, function(err) {
     if (err) {
       return console.error(err);
     }
-    console.log("appending in file 2 complete");
+    console.log('appending in file 2 complete');
     readFile2();
   });
 };
 
 var readFile2 = function() {
-  fs.readFile("file2.txt", function(err, data) {
+  fs.readFile('file2.txt', function(err, data) {
     if (err) {
       return console.error(err);
     }
-    console.log("Reading file 2 complete");
+    console.log('Reading file 2 complete');
     writefile3(data);
   });
 };
 
 var writefile3 = function(data) {
-  fs.writeFile("file3.txt", data, function(err) {
+  fs.writeFile('file3.txt', data, function(err) {
     if (err) {
       return console.error(err);
     }
-    console.log("writing in file 3 complete");
+    console.log('writing in file 3 complete');
   });
 };
